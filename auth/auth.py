a33c972f83f513c13a4c672a0637d7e07547514d
Sainath <sainath.meduri@gmail.com>
2020-05-24 23:17:10 +0530

    Update auth.py as per review comments

--- auth/auth.py
+++ auth/auth.py
@@ -5,10 +5,6 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN='sainath-fsnd.auth0.com'
-ALGORITHMS=['RS256']
-API_AUDIENCE='CapstoneAPI'
-
 # AuthError Exception
 '''
 AuthError Exception
